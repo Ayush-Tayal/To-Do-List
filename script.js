67c5d160aad5c0269b081985a951130d3cbbe27a
Ayush Tayal <ayushtayal1111@gmail.com>
2021-10-25 17:17:48 +0530

    update

--- script.js
+++ script.js
@@ -3,18 +3,36 @@ const button = document.querySelector("#addTodoBtn");
 const ol = document.querySelector("#todoList")
 // console.log(input, button, ol);
 
-button.addEventListener("click", ()=>{
+button.addEventListener("click",addtodo);
+ 
+ol.addEventListener("click",olEventListener);
+
+function addtodo(){
     if(input.value == "") return;
 
     const todo = document.createElement("li");
     todo.innerText = input.value;
     ol.appendChild(todo);
 
+    //css on todo
+    todo.style.margin = "10px";
+    todo.style.padding = "5px";
+  
     // del button
     const del = document.createElement("button");
     del.innerText = "Delete";
     todo.appendChild(del);
+    
+    // css
+    del.style.marginLeft = "10px";
 
     input.value = "";
-})
+}
 
+function olEventListener(e){
+    if(e.target.innerText === "Delete")
+    {
+        // console.log(e.target.innerText);
+        e.target.parentNode.remove();
+    }
+}
